5e0d78e3478f558b8cd2a7ba5b85cb18edbbc2c4
KAMBALE NGUNUNU DANIEL <163008822+KAMBALENGUNUNU@users.noreply.github.com>
2025-04-01 15:00:11 +0200

    Add files via upload
    added the necessary files

---
+++ Danny/SignUpViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Group_work.Models
+{
+    public class SignUpViewModel
+    {
+        [Required(ErrorMessage = "Full name is required")]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Phone(ErrorMessage = "Invalid phone number")]
+        [Display(Name = "Phone (Optional)")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords don't match")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
